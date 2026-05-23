574128f9f12983abfa9bc37c80299e5cff5bf9da
MURARI VAMSHARAJ <155216940+murarivamsharaj@users.noreply.github.com>
2026-05-23 14:56:30 +0530

    ✅ Add solution for: Orders Per Customer Count - SQL

---
+++ exercises/OrdersPerCustomerCountSQL.sql
@@ -0,0 +1,13 @@
+-- Write a SQL query below --
+SELECT 
+    c.first_name,
+    c.last_name,
+    COUNT(o.id) AS order_count
+FROM 
+    customers c
+INNER JOIN 
+    orders o ON c.id = o.customer_id
+GROUP BY 
+    c.id, c.first_name, c.last_name
+ORDER BY 
+    order_count DESC;
